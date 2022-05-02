353626d9329b2c67dd40429a37b2f35fdcac438f
hugsbord <hugsbord@student.42.fr>
2022-05-02 18:03:57 +0200

    first commit + login screen

---
+++ firebase-auth/login_auth/screens/home_screen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home_screen = () => {
+    return (
+        <View>
+            <Text>Home screen</Text>
+        </View>
+    )
+}
+
+export default Home_screen
+
+const styles = StyleSheet.create({})
